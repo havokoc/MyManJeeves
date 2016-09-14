e088b85ac06948b435d17c5551d938f59cbffc4c
TantanD <jonatan@davidssons.com>
2016-09-14 19:23:43 +0200

    Added support for .JSON files + links.JSON

--- Jeeves/jeeves.py
+++ Jeeves/jeeves.py
@@ -2,6 +2,7 @@ import discord
 import asyncio
 import random
 import configparser
+import json
 
 def RunBot(config_file):
     config = configparser.ConfigParser()
@@ -19,19 +20,7 @@ def RunBot(config_file):
     async def on_message(message):
         if message.channel.id == "123410749765713920":
             if message.content.startswith('-knugen'):
-                knugenLinks = [
-                "http://i.imgur.com/dG0q4a9.png",
-                "http://i.imgur.com/pCtYLDP.jpg",
-                "http://i.imgur.com/QnvhnT8.jpg",
-                "http://i.imgur.com/rjxCi4a.jpg",
-                "http://i.imgur.com/mpSoTfk.png",
-                "http://i.imgur.com/Jy3g2Q6.jpg",
-                "http://i.imgur.com/bpwp3rb.jpg",
-                "http://i.imgur.com/Y0ylzT5.jpg",
-                "http://i.imgur.com/AlC76nS.png",
-                "http://i.imgur.com/fkAAgh1.gifv",
-                "https://i.imgur.com/qdzHLJA.jpg",
-                ]
+                
                 await client.send_message(message.channel, random.choice(knugenLinks))
 
     client.run(config['Bot']['token'])
