9973d3deff0e206fe47fda033daea78b26bf1f6e
Havok <havok@havokoc.se>
2017-04-26 16:16:52 +0200

    Added comments

--- Jeeves/jeeves.py
+++ Jeeves/jeeves.py
@@ -6,9 +6,9 @@ import configparser
 import json
 import logging
 from datetime import datetime
-#HOJJ
 
-#Loggingclassen som ska användas för resen av alla commands
+
+#Loggingclassen som ska användas för resten av alla commands
 class loggingFunc(object):
     def __init__(self, command, user):
         self.command = command
@@ -32,41 +32,42 @@ def RunBot(config_file):
     @client.event
     async def on_message(message):
         if message.channel.id == "123410749765713920" or "139430532437114880":
-            #Command - Knugen
+            '''
+            Command - Knugen
+            !knugen
+            Postar en bild på moderrikets konung i en embed.
+            '''
             if message.content.startswith('%sknugen' % config['Bot']['prefix']):
                 with open('config/data.json') as data_file:
                     data = json.loads(data_file.read())
-                    #await client.send_message(message.channel, random.choice(data['knugenLinks']))
                     embed = discord.Embed(title='Välfärden is here again!', colour=0xEDC922)
                     embed.set_image(url=random.choice(data['knugenLinks']))
                     await client.send_message(message.channel, embed=embed)
                     print(loggingFunc("knugen", message.author.name))
-            #Command - Avatar
+
+            '''
+            Command - Avatar
+            !avatar <user>
+            Outputtar den specifierade användarens avatar. Om användaren inte har
+            specifierat någon annan användare, visa användarens avatar i en embed.
+            '''
             if message.content.startswith('%savatar' % config['Bot']['prefix']):
                 if len(message.content.split()) < 2:
                     embed = discord.Embed(title='%s\'s avatar' % message.author.name, colour=0xEDC922)
-                    #embed.set_image(url='https://cdn.discordapp.com/avatars/'+message.author.id+'/'+message.author.avatar+'.gif')
                     embed.set_image(url=message.author.avatar_url)
                     await client.send_message(message.channel, embed=embed)
                     print(loggingFunc("avatar", message.author.name))
                 else:
                     embed = discord.Embed(title='%s\'s avatar' % message.mentions[0].name, colour=0xEDC922)
-                    #embed.set_image(url='https://cdn.discordapp.com/avatars/'+message.mentions[0].id+'/'+message.mentions[0].avatar+'.gif')
                     embed.set_image(url=message.mentions[0].avatar_url)
                     await client.send_message(message.channel, embed=embed)
                     print(loggingFunc("avatar", message.author.name))
-            if message.content.startswith('%shonk' % config['Bot']['prefix']):
-                if message.author.voice.voice_channel == None:
-                    await client.send_message(message.channel, "You are not in a voice channel.")
-                else:
-                    voice = await client.join_voice_channel(message.author.voice.voice_channel)
-                    player = await voice.create_ytdl_player('https://www.youtube.com/watch?v=4Iut-7RzwkQ')
-                    player.start()
-                    player.stop()
-                    voice.disconnect()
-            #Command - X
 
     client.run(config['Bot']['token'])
 
+'''
+Kräver att man startar med start.py som ligger i huvudmappen, då denna
+låter en specifiera en config fil som inte liggar i default mappen.
+'''
 if __name__ == "__main__":
     print("Please use the start.py script in the root directory instead")
